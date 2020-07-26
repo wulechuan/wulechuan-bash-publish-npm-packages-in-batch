f37907e38089e3836348fcdddc63566abefb7582
吴乐川 <wulechuan@live.com>
2020-07-26 11:38:36 +0800

    已改用 curl 从 taobao 注册点拉去现成的 tgz。

--- source/search-a-folder-and-publish-all.sh
+++ source/search-a-folder-and-publish-all.sh
@@ -25,6 +25,7 @@ function searchRecursivelyAndPublishAll {
     # ───────────────────────────────────────────────────────────────────────────
 
 
+    mkdir -p ~/taobao-npm-tgz-caches
 
     echo -e "\e[32mSearching from \"\e[35m${searchingRootPath}\e[32m\"...\e[0m"
     local allNodeModulesFolders=(`find  ${searchingRootPath} -name 'node_modules'`)
@@ -54,11 +55,13 @@ function searchRecursivelyAndPublishAll {
         local a_pack_path
         local pack_index=0
 
+        local package_name_prefix=''
+
         for a_pack_path in ${packPaths[@]}; do
             local a_pack_path_string_length=${#a_pack_path}
             local a_pack_path_sliced_length=$((a_pack_path_string_length-1))
             local a_pack_path_no_slash_suffix=${a_pack_path:0:a_pack_path_sliced_length}
-            local package_name=$a_pack_path_no_slash_suffix
+            local package_folder_name=$a_pack_path_no_slash_suffix
 
             local package_full_path="${a_node_modules_path}/${a_pack_path_no_slash_suffix}"
 
@@ -70,25 +73,34 @@ function searchRecursivelyAndPublishAll {
             echo -e  "            package: \e[35m${pack_index}\e[0m/\e[33m${foundPacksCount}\e[0m"
             echo -e  "${VE_line_60}"
 
+
             if [ ! -f "${packageJSONFullPath}" ]; then
-                echo -e "\e[31mINVALID (thus ignored)\e[0m: \e[32m${package_name}\e[0m"
+                if [[ "${package_folder_name}" =~ ^@[_a-z0-9]+\/ ]]; then
+                    echo
+                    package_folder_name_prefix=${package_folder_name}\/
+                else
+                    echo -e "\e[31mINVALID (thus ignored)\e[0m: \e[32m${package_folder_name}\e[0m"
+                    package_folder_name_prefix=''
+                fi
             else
                 local packageVersionLines=`cat "${packageJSONFullPath}" | grep "^\s*\"version\":\s*\"[0-9]\+\.[0-9]\+\."`
                 local package_version=`echo "${packageVersionLines}" | sed '/"[0-9\.]\+/!d' | sed 's/ \+"version": \+"//' | sed 's/",\? *$//'`
                 # echo "package_version=\"${package_version}\""
 
-                echo -e "Package in the \e[33mnode_modules\e[0m folder:   \e[32m${package_name}\e[0m@\e[35m${package_version}\e[0m"
+                local package_full_name="${package_folder_name_prefix}${package_folder_name}"
+
+                echo -e "Package in the \e[33mnode_modules\e[0m folder:   \e[32m${package_folder_name}\e[0m@\e[35m${package_version}\e[0m"
                 echo -e  "${VE_line_60}"
 
                 npm  search\
                     --no-description\
-                    --parseable "${package_name}"\
+                    --parseable "${package_full_name}"\
                     --registry="${npmRegistryURL}"\
                     | sed  's/\t[^\t]\+$//g'\
                     | sed  's/\t=[^\t]\+//g'\
                     | sed  's/\t[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}//'\
                     | sed  's/\t$//'\
-                    | grep "^${package_name}\s"\
+                    | grep "^${package_full_name}\s"\
                     > ~/wlc-npm-search-result.tmp
 
                 local searchingResult=`head -n 1 ~/wlc-npm-search-result.tmp`
@@ -101,21 +113,32 @@ function searchRecursivelyAndPublishAll {
                     shouldPublish=1
                 else
                     local searchingResultVersion=`echo "${searchingResult}" | sed 's/^[^\t]\+\t//'`
-                    # echo -e "\e[30;42m${npmRegistryURL}\e[0;0m \e[32mFOUND VERSION: \e[33m${package_name}\e[0m@\e[35m${searchingResultVersion}\e[0m"
+                    # echo -e "\e[30;42m${npmRegistryURL}\e[0;0m \e[32mFOUND VERSION: \e[33m${package_full_name}\e[0m@\e[35m${searchingResultVersion}\e[0m"
 
                     if [ "${package_version}" == "${searchingResultVersion}" ]; then
-                        echo -e "\e[30;41m${npmRegistryURL}\e[0;0m \e[31mALREADY EXISTS: \e[32m${package_name}\e[0m@\e[35m${searchingResultVersion}\e[0m"
+                        echo -e "\e[30;41m${npmRegistryURL}\e[0;0m \e[31mALREADY EXISTS: \e[32m${package_full_name}\e[0m@\e[35m${searchingResultVersion}\e[0m"
                     else
-                        echo -e "\e[30;42m${npmRegistryURL}\e[0;0m \e[34mEXISTS ANOTHER VERSION: \e[34m${package_name}\e[0m@\e[35m${searchingResultVersion}\e[0m"
+                        echo -e "\e[30;42m${npmRegistryURL}\e[0;0m \e[34mEXISTS ANOTHER VERSION: \e[34m${package_full_name}\e[0m@\e[35m${searchingResultVersion}\e[0m"
                         shouldPublish=1
                     fi
                 fi
 
                 if [ $shouldPublish -eq 1 ]; then
+                    local taobao_tgz_url="https://registry.npm.taobao.org/${package_full_name}/download/${package_full_name}-${package_version}.tgz"
+
                     echo -e  "\e[32m${VE_line_50}\e[0m"
-                    echo -e "\e[30;42mnpm publishing\e[0;0m \e[32m${package_name}\e[0m@\e[35m${package_version}\e[0m..."
+                    echo -e  "\e[30;42mdownloading tgz from taobao\e[0;0m \e[32m${package_full_name}\e[0m@\e[35m${package_version}\e[0m..."
                     echo -e  "\e[32m${VE_line_50}\e[0m"
-                    npm  publish  --registry="${npmRegistryURL}"  ${package_full_path}
+                    echo -e  "RESOURCE: \e[32m${taobao_tgz_url}\e[0m"
+
+                    curl -L "${taobao_tgz_url}" > ~/taobao-npm-tgz-caches/"${package_full_name}-${package_version}.tgz"
+
+                    echo -e  "\e[32m${VE_line_50}\e[0m"
+
+                    echo -e "\e[30;42mnpm publishing\e[0;0m \e[32m${package_full_name}\e[0m@\e[35m${package_version}\e[0m..."
+                    echo -e  "\e[32m${VE_line_50}\e[0m"
+                    # npm  publish  --registry="${npmRegistryURL}"  ${package_full_path}
+                    npm  publish  --registry="${npmRegistryURL}"  ~/taobao-npm-tgz-caches/"${package_full_name}-${package_version}.tgz"
                     echo -e  "\e[32m${VE_line_50}\e[0m"
                 fi
             fi
